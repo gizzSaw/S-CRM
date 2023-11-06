400a3f4d07c2b3c8e58784ab1e89babe24379bc7
gizzSaw <gizzsaw@gmail.com>
2023-11-06 08:44:13 +0300

    footer

--- CRM/app/include/header.php
+++ CRM/app/include/header.php
@@ -3,24 +3,24 @@
             <div class="row">
                 <div class="col-4">
                     <h1>
-                        <a href="index.php">
+                        <a href="<?php echo BASE_URL?>">
                             My blog
                         </a>
                     </h1>
                 </div>
                 <nav class="col-8">
                     <ul>
-                        <li><a href="#">Главная</a></li>
-                        <li><a href="#">О нас</a></li>
-                        <li><a href="#">Услуги</a></li>
+                        <li><a href="<?php echo BASE_URL?>">Главная</a></li>
+                        <li><a href="<?php echo BASE_URL . 'about.php'?>">О нас</a></li>
+                        <li><a href="<?php echo BASE_URL . 'services.php'?>">Услуги</a></li>
                         <li>
-                            <a href="#">
+                            <a href="<?php echo BASE_URL?>">
                                 <i class="fa fa-user">
                                 </i>Кабинет
                             </a>
                             <ul>
-                                <li><a href="#">Админ панель</a></li>
-                                <li><a href="#">Выход</a></li>
+                                <li><a href="log.php">Админ панель</a></li>
+                                <li><a href="<?php echo BASE_URL?>">Выход</a></li>
                             </ul>
                         </li>
                     </ul>
