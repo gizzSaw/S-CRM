d8f0db7305b3f5990021b620d741867ea3c560fc
gizzSaw <gizzsaw@gmail.com>
2023-11-07 20:17:48 +0300

    where, and

--- CRM/app/database/db.php
+++ CRM/app/database/db.php
@@ -3,29 +3,84 @@
 require('connect.php');
 
 
-//функция для респечатывания массива
+// для респечатывания массива
 function tt($value) {
     echo '<pre>';
     print_r($value);
     echo '</pre>';
 }
-
-function selectAll($table){
-    global $pdo;
-    $sql = "SELECT * FROM $table";
-    $query = $pdo->prepare($sql);   //подготовка
-    $query->execute();              //обновление
-    $errInfo = $query->errorInfo(); //возврат ошибок если есть
-
+// проверки запроса к БД
+function dbCheckError($query){
+    $errInfo = $query->errorInfo();         //возврат ошибок если есть
     if ($errInfo[0] !== PDO::ERR_NONE){
         echo $errInfo[2];
         exit();
     }
+    return true;
+}
+//запрос на получение данных
+function selectAll($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
 
+    if (!empty($params)){ //если параметры есть 
+        $i = 0;
+        foreach ($params as $key => $value){ 
+            if (!is_numeric($value)) {          //если параметр не число
+                $value = "'".$value."'";        //добавить кавычки
+            }
+            if ($i === 0) {
+                $sql = $sql . " WHERE $key = $value "; //первый подставляем через WHERE
+            }  else {
+                $sql = $sql . " AND $key = $value";    //остальные подставляем через AND
+            }
+            $i++;
+        }
+        
+    }
+    
+    $query = $pdo->prepare($sql);          //подготовка
+    $query->execute();                     //обновление
+    dbCheckError($query);                  //возврат ошибок если есть
     return $query->fetchAll();;
 }
 
-tt(selectAll("users"));
+//запрос на получение одной строки из выбраной таблицы
+function selectOne($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+
+    if (!empty($params)){ //если параметры есть 
+        $i = 0;
+        foreach ($params as $key => $value){ 
+            if (!is_numeric($value)) {          //если параметр не число
+                $value = "'".$value."'";        //добавить кавычки
+            }
+            if ($i === 0) {
+                $sql = $sql . " WHERE $key = $value "; //первый подставляем через WHERE
+            }  else {
+                $sql = $sql . " AND $key = $value";    //остальные подставляем через AND
+            }
+            $i++;
+        }
+    }
+
+    $sql = $sql . "LIMIT 1";               //можно убрать, так как в конце fetch
+    $query = $pdo->prepare($sql);          //подготовка
+    $query->execute();                     //обновление
+    dbCheckError($query);                  //возврат ошибок если есть
+    return $query->fetch();;
+}
+
+
+// КОД ДЛЯ ТЕСТИРОВАНИЯ
+$params = [
+    'admin' => 1
+];
+
+
+tt(selectOne("users", $params));
+tt(selectAll("users", $params));
 
 
 
